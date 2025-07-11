e889e6053a9ede1290c55c7d9209e5fba34ece24
zerlands51 <zerlands51@gmail.com>
2025-07-11 18:34:55 +0700

    Updated supabase.ts

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -46,6 +46,8 @@ export interface Database {
           company?: string | null
           created_at?: string
           updated_at?: string
+          email_confirmation_token?: string | null
+          email_confirmation_sent_at?: string | null
         }
       }
       advertiser_accounts: {
